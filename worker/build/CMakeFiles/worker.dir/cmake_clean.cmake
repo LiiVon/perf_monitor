f64b7f262f7652eb08021fb11e46b9de5d6ee970
LiiVon <2671749518@qq.com>
2026-07-27 20:23:33 +0800

    首次提交项目代码

---
+++ worker/build/CMakeFiles/worker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/worker.dir/src/utils/read_file.o"
+  "CMakeFiles/worker.dir/src/utils/read_file.o.d"
+  "worker"
+  "worker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/worker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
